bce618c1aff1437ca6493a3625e207771d53b9e1
drewtess069 <drewtess069@ed.amdsb.ca>
2022-05-17 14:37:51 -0400

    Added background

--- spaceRace/Form1.Designer.cs
+++ spaceRace/Form1.Designer.cs
@@ -94,6 +94,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(600, 600);
             this.Controls.Add(this.subTitleLabel);
             this.Controls.Add(this.titleLabel);
